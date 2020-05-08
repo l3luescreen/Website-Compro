12792f7b7ddfd5b322b14495cdfb362955cc1b50
l3luescreen <hacky404@hotmail.com>
2020-05-08 13:39:17 +0700

    change name
    Hermes to Hermez

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,38 +7,49 @@ import '../style/Navbar.css';
 
 class Nav extends React.Component {
     state = {
-        current: <Homepage />,
+        current: < Homepage / > ,
     };
 
     homePage = () => {
         console.log("click");
-        this.setState({ current: <Homepage /> });
+        this.setState({ current: < Homepage / > });
     };
     membersPage = () => {
         console.log("click");
-        this.setState({ current: <Member /> });
+        this.setState({ current: < Member / > });
     };
     render() {
-        return (
-            <div className="navbar">
-                <nav className="navbar navbar-expand-xl navbar-dark bg-dark justify-content-center navbar-toggler fixed-top">
-                    <a className="navbar-brand" href="#">Project Hermes</a>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={this.homePage}>Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={this.membersPage}>members</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                    <div className="page">
-                        {this.state.current}
-                    </div>
-                    <Bottom />
-                </div>
+        return ( <
+            div className = "navbar" >
+            <
+            nav className = "navbar navbar-expand-xl navbar-dark bg-dark justify-content-center navbar-toggler fixed-top" >
+            <
+            a className = "navbar-brand"
+            href = "#" > Project Hermez < /a> <
+            div className = "collapse navbar-collapse"
+            id = "navbarNav" >
+            <
+            ul className = "navbar-nav" >
+            <
+            li className = "nav-item" >
+            <
+            a className = "nav-link"
+            onClick = { this.homePage } > Home < span className = "sr-only" > (current) < /span></a >
+            <
+            /li> <
+            li className = "nav-item" >
+            <
+            a className = "nav-link"
+            onClick = { this.membersPage } > members < /a> <
+            /li> <
+            /ul> <
+            /div> <
+            /nav> <
+            div className = "page" > { this.state.current } <
+            /div> <
+            Bottom / >
+            <
+            /div>
         );
     }
 }
